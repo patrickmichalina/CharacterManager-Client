d132bb96ecefac1dc9b62bb24594a4dcf785d3cd
Patrick <patrickmichalina@mac.com>
2015-10-22 00:15:40 -0700

    fix(repo)

--- typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="angular-material/angular-material.d.ts" />
-/// <reference path="angular-ui-router/angular-ui-router.d.ts" />
-/// <reference path="angularjs/angular-resource.d.ts" />
-/// <reference path="angularjs/angular.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
