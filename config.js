77258bfd4291890ebfdefe78658bcf0ae8395e40
Patrick <patrickmichalina@mac.com>
2015-10-21 18:06:13 -0700

    chore(packages)

--- config.js
+++ config.js
@@ -9,9 +9,25 @@ System.config({
 
   map: {
     "angular": "github:angular/bower-angular@1.4.7",
+    "angular-material": "github:angular/bower-material@0.11.4",
+    "angular-messages": "github:angular/bower-angular-messages@1.4.7",
+    "angular-resource": "github:angular/bower-angular-resource@1.4.7",
+    "material-design-iconic-font": "npm:material-design-iconic-font@2.1.2",
     "traceur": "github:jmcriffey/bower-traceur@0.0.92",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.92",
     "ui-router": "npm:ui-router@0.2.15",
+    "github:angular/bower-angular-animate@1.4.7": {
+      "angular": "github:angular/bower-angular@1.4.7"
+    },
+    "github:angular/bower-angular-aria@1.4.7": {
+      "angular": "github:angular/bower-angular@1.4.7"
+    },
+    "github:angular/bower-material@0.11.4": {
+      "angular": "github:angular/bower-angular@1.4.7",
+      "angular-animate": "github:angular/bower-angular-animate@1.4.7",
+      "angular-aria": "github:angular/bower-angular-aria@1.4.7",
+      "css": "github:systemjs/plugin-css@0.1.19"
+    },
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+var gulp = require('gulp');
+var rename = require("gulp-rename");
+var browserSync = require('browser-sync').create();
+var historyApiFallback = require('connect-history-api-fallback');
+var htmlreplace = require('gulp-html-replace');
+var replace = require('gulp-replace');
+var gzip = require('gulp-gzip');
+var minifyHTML = require('gulp-minify-html');
+var uglify = require('gulp-uglify');
+var deployCdn = require('gulp-deploy-azure-cdn');
+var gutil = require('gulp-util');
+
+
+gulp.task('bundle', function (cb) {
+	var Builder = require('systemjs-builder');
+	var builder = new Builder();
+	builder.reset();
+
+	builder.loadConfig("./config.js")
+		.then(function () {
+			return builder.buildSFX("app", "dist/build.min.js", { minify: false })
+				.then(function () { // Inject minified script into index
+					return gulp.src('dist/index.html')
+						.pipe(htmlreplace({ 'js': 'build.min.js' }))
+						//.pipe(htmlreplace({ 'js': cdn + '/app/build.min.js' }))
+						.pipe(minifyHTML())
+						.pipe(gulp.dest('dist/'));
+				});
+		})
+		.then(function () {
+			cb();
+		})
+		.catch(function (err) {
+			console.log(err);
+			cb(err);
+		});
+});
+
+gulp.task('index', function () {
+	return gulp.src('index.html')
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('basic-bundle', function () {
+	return gulp.src(['favicon.ico', 'robots.txt'])
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: "./",
+		middleware: [historyApiFallback()],
+		browser: "google chrome",
+		files: ["./app/**/*.*"]
+    });
+});
+
+gulp.task('md-icons', function(){
+	return gulp.src(['jspm_packages/*/material-design-iconic-font*/dist/fonts/**.*'], { "base" : "." })
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function(){
+	return gulp.src('app/images/**/**.*')
+		.pipe(gulp.dest('dist/app/images'));
+});
+
+gulp.task('compress', function() {
+  return gulp.src('dist/build.min.js')
+	//.pipe(replace('url(jspm', 'url(' + cdn + '/app/jspm'))
+	//.pipe(replace('app/images', cdn + '/app/images'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist'));
+});
+
+
+
+gulp.task('dist', 
+	gulp.series('index', 
+		gulp.parallel('bundle', 'basic-bundle', 'images', 'md-icons'), 
+		'compress', 
+		gulp.parallel('upload-favicon-to-azure')));
+
+gulp.task('serve-dist',
+	gulp.series('dist', function () {
+		browserSync.init({
+			server: "./dist",
+			middleware: [historyApiFallback()],
+			browser: "google chrome",
+			minify: true
+		})})
+);
+
+gulp.task('default', gulp.series('serve'));
