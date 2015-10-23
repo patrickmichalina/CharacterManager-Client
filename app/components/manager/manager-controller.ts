089ccd8dcab2c9d7d02eb19b1f9bc36171ef8c08
Patrick <patrickmichalina@mac.com>
2015-10-23 08:48:43 -0700

    fix(component): modal now creates character and shows asap

--- app/components/manager/manager-controller.ts
+++ app/components/manager/manager-controller.ts
@@ -1,6 +1,5 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 import {IDataAccessService} from '../../common/common-interfaces';
-
 import modalController from './create-modal/create-modal-controller';
 import modalTemplate from './create-modal/create-modal-template.html!text';
 
@@ -9,16 +8,20 @@ export default class ManagerController {
 	public characters = [];
 	public selected = [];
 	public selectedForRestore = [];
+	public factions = [];
+	public races = [];
+	public classes = [];
 	public viewDeleted = false;
+	private test = this.$mdDialog.hide;
 
-	static $inject = ['DataAccessService', '$mdToast', '$mdDialog'];
+	static $inject = ['DataAccessService', '$mdToast', '$mdDialog', '$scope', '$state', '$stateParams'];
 
-	resetSelected(){
+	resetSelected() {
 		this.selected = [];
 		this.selectedForRestore = [];
 	}
 
-	constructor(private DataAccessService: IDataAccessService, private $mdToast, private $mdDialog) {
+	constructor(private DataAccessService: IDataAccessService, private $mdToast, private $mdDialog, private $scope, private $state, private $stateParams) {
 		this.getCharacters();
 	}
 
@@ -35,6 +38,7 @@ export default class ManagerController {
 			this.deleteCharacter(character.Name)
 		}
 		this.resetSelected();
+		this.showDeletedCharacterAlert(selected.length);
 	}
 
 	deleteCharacter(characterName) {
@@ -47,13 +51,13 @@ export default class ManagerController {
 			},
 			(err) => { this.error(err) });
 	}
-	
-	getDisabledCount(){
+
+	getDisabledCount() {
 		let count = 0;
-		for(var character of this.characters){
-			if(character.IsDeleted === true) count++;
+		for (var character of this.characters) {
+			if (character.IsDeleted === true) count++;
 		}
-		
+
 		return count;
 	}
 
@@ -73,6 +77,7 @@ export default class ManagerController {
 			this.restoreCharacter(character.Name)
 		}
 		this.toggleShowDeleted();
+		this.showRestoredCharacterAlert(selected.length);
 	}
 
 	toggleShowDeleted() {
@@ -81,33 +86,88 @@ export default class ManagerController {
 		this.viewDeleted = !this.viewDeleted;
 	}
 
+	onCreated(response) {
+		var test = this;
+		//debugger;
+    }
 
-
-	showCreateCharacterDialog(ev) {
-
-
-		this.$mdDialog.show({
-			parent: angular.element(document.body),
-			controller: modalController,
-			template: modalTemplate,
-			targetEvent: ev,
-			clickOutsideToClose: true
-		})
-			.then(function(answer) {
-
-			}, function() {
-
-			});
-
+	showDeletedCharacterAlert(numDeleted) {
+		this.$mdToast.show(this.$mdToast.simple().content('Deleted ' + numDeleted + ' characters'));
 	}
 
-
+	showRestoredCharacterAlert(numRestored) {
+		this.$mdToast.show(this.$mdToast.simple().content('Deleted ' + numRestored + ' characters'));
+	}
 
 	showCreatedCharacterAlert() {
 		this.$mdToast.show(this.$mdToast.simple().content('Your character was created! Have fun :)'));
 	}
 
 	error(error) {
+		this.$mdToast.show(this.$mdToast.simple().content(error.data.Message));
+	}
+	
+	///////////////////// MODAL
+	
+	showCreateCharacterDialog(ev) {
+		this.$mdDialog.show({
+			targetEvent: ev,
+			locals: { parent: this },
+			controller: angular.noop,
+			controllerAs: 'ctrl',
+			bindToController: true,
+			template: modalTemplate
+		})
+			.then(this.onCreated, this.onCreated);
+	}
+
+	getFactions = () => {
+		return this.DataAccessService.getFactionResource().getAll().$promise.then(
+			(response) => {
+				this.factions = response;
+			},
+			(err) => { this.error(err) });
+	}
+
+	getRaces = () => {
+		return this.DataAccessService.getRaceResource().getAll().$promise.then(
+			(response) => {
+				this.races = response;
+			},
+			(err) => { this.error(err) });
+	}
+
+	getClasses = () => {
+		return this.DataAccessService.getClassResource().getAll().$promise.then(
+			(response) => {
+				this.classes = response;
+			},
+			(err) => { this.error(err) });
+	}
+
+	hide = function() {
+		this.$mdDialog.hide();
+	};
+
+	cancel = function() {
+		this.$mdDialog.cancel();
+	};
+
+	create = function(character) {
+		return this.DataAccessService.getCharacterResource().create(character).$promise.then(
+			(response) => {
+				this.characters.push(character);
+				this.hide();	
+				this.$state.transitionTo(this.$state.current, this.$stateParams, {
+    				reload: true,
+    				inherit: false,
+    				notify: true
+				});
+			},
+			(err) => { this.errorW(err) });
+	}
+
+	errorW = (error) => {
 		this.$mdToast.show(this.$mdToast.simple().content(error.data.Message));
 	}
 }
